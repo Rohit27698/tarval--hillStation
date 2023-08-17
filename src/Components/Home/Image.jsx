48daa6b54e313a9707a0bfe9f7735a263f6a7e4a
Bharatveersingh-K <bharatveersingh271@gmail.com>
2023-08-17 11:27:22 +0530

    Home_Page_Created

---
+++ src/Components/Home/Image.jsx
@@ -0,0 +1,33 @@
+const ourTeam = [
+    {
+      name: "Rohit Kharnar",
+      position: "Full Stack Developer",
+      company: "Zentech",
+      image: "https://masai-website-images.s3.ap-south-1.amazonaws.com/prateek_6232b84d8b.jpeg"  
+    },
+    {
+        name: "Indraeet Gupta",
+        position: "Full Stack Android Developer",
+        company: "Airtel",
+        image: "https://masai-website-images.s3.ap-south-1.amazonaws.com/prateek_6232b84d8b.jpeg"  
+      },
+      {
+        name: "Bharatveer Singh",
+        position: "Frontend Developer",
+        company: "Wipro",
+        image: "blob:https://web.whatsapp.com/4c9c514b-f52a-4676-97c9-cb30a181d08e"  
+      },
+      {
+        name: "Saif Khan",
+        position: "Backend Developer",
+        company: "SISCO",
+        image: "https://masai-website-images.s3.ap-south-1.amazonaws.com/prateek_6232b84d8b.jpeg"  
+      },
+      {
+        name: "Asif Wali",
+        position: "Sr. Software-Developer",
+        company: "Udacity",
+        image: "https://masai-website-images.s3.ap-south-1.amazonaws.com/prateek_6232b84d8b.jpeg"  
+      },
+    
+  ];
